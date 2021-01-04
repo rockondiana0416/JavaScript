e84bf64e13bf324eb03cc0f217c35d6dfffd6068
Diana <rockondiana0416@yahoo.com>
2021-01-03 22:55:05 -0800

    Madlibs made with js prompts

---
+++ madlibs-prompts/script.js
@@ -0,0 +1,9 @@
+var yourName = prompt("Enter your name: ");
+var occupation = prompt("Enter an occupaton: ");
+var salary = prompt("Enter a number between 2 and 100: ");
+var yourExp = prompt("Enter a saying or expression: ");
+var verb = prompt("Enter an action verb not ending in ING: ");
+var adjective = prompt("Enter an adjective: ");
+var pluralNoun = prompt("Enter a plural noun: ");
+
+document.write(yourName + " just got offered a new job as a " + occupation + " making " + salary + " dollars per hour. " + yourExp + " you say! Your job requiers you to " + verb + " and make " + adjective + " " + pluralNoun + ".");
